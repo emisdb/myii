883c4e1e0c3396d419bf54a233769b6215b2f997
Denis <dentsi@yahoo.com>
2015-09-19 16:35:38 +0300

    3d

---
+++ protected/views/site/pages/d3.php
@@ -0,0 +1,14 @@
+<h1>D3.JS Experiments</h1>
+<div id="xsvg">
+    <svg width="550" height="300" style="border: #000 dotted thin; background-color: #fff;">
+	<line x1="0" y1="0" x2="550" y2="300" style="stroke:#8f0; stroke-width:3;"  />
+		<circle ondblclick="alert('what the fuck!')" 
+			cx="50" cy="50" r="75" style="fill:#bbb;stroke:#088;stroke-width:3;fill-opacity:0.9;" />
+		<rect x="130" y="80" width="80" height="80" rx="3" ry="10" style="fill:#f0f; stroke-width:3; stroke:#454;fill-opacity:0.2; stroke-opacity:0.9;" />
+		<ellipse cx="280" cy="60" rx="60" ry="35" style="fill:#68f; stroke-width:3;stroke:#f86;" />
+		<a xlink:href="<?php echo(Yii::app()->request->baseUrl."/index.php?r=project/admin");?>">
+		<polygon points="40,200 80,200 100,250 20,250" style="fill:-webkit-linear-gradient(bottom ,#363 0%,#4a4 100%) repeat scroll 0 0 transparent;" />
+		</a>
+    </svg>
+    
+</div>

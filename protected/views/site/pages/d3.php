3e5dac394b7b2c46dd3a58c9f44b22656543a7a7
Denis <dentsi@yahoo.com>
2015-09-19 16:39:14 +0300

    d3

--- protected/views/site/pages/d3.php
+++ protected/views/site/pages/d3.php
@@ -6,7 +6,7 @@
 			cx="50" cy="50" r="75" style="fill:#bbb;stroke:#088;stroke-width:3;fill-opacity:0.9;" />
 		<rect x="130" y="80" width="80" height="80" rx="3" ry="10" style="fill:#f0f; stroke-width:3; stroke:#454;fill-opacity:0.2; stroke-opacity:0.9;" />
 		<ellipse cx="280" cy="60" rx="60" ry="35" style="fill:#68f; stroke-width:3;stroke:#f86;" />
-		<a xlink:href="<?php echo(Yii::app()->request->baseUrl."/index.php?r=project/admin");?>">
+		<a xlink:href="<?php echo(Yii::app()->request->baseUrl."/index.php?r=project/admin");?>"> 
 		<polygon points="40,200 80,200 100,250 20,250" style="fill:-webkit-linear-gradient(bottom ,#363 0%,#4a4 100%) repeat scroll 0 0 transparent;" />
 		</a>
     </svg>
